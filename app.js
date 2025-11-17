1543273723051c250cbc016c0c18081925d54c48
Mirwais <mirwais.sanaie@gmail.com>
2025-11-17 22:41:36 +0430

    ...

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+
+// Middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//my own
+app.use((req, res, next) => {
+  console.log("Hello from the middleware 👋");
+  next();
+});
+
+//
+
+//route to access .html .img .css or ...
+app.use(express.static(`${__dirname}/public`));
+// app middlewares
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
---
+++ controllers/routeController.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is : ${val}`);
+
+  if (val * 1 > tours.length) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+    return;
+  }
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    success: true,
+    results: tours.length,
+    data: tours,
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    success: true,
+    data: tour,
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (error) => {
+      res.status(201).json({
+        success: true,
+        data: newTour,
+      });
+    }
+  );
+
+  res.status(201).json({
+    success: true,
+    message: "Done",
+  });
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Update tour here...",
+  });
+};
